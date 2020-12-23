83ddf07862ab9ce2ec35a27052a17db24a1caf82
zach6585 <zmines@wesleyan.edu>
2020-12-22 21:20:37 -0500

    commit tree d047c7bd7c277da31ed2100052768591e6fbbd5f

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded")
+    let text = document.getElementById("text")
+    text.innerHTML = "This is really cool!"
+});
+
